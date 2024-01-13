c6321fb1901b70d2cf6ec2fde69d1b3be86177e3
Machi-san <maxibengochea67@gmail.com>
2024-01-13 15:54:29 -0500

    client test

---
+++ trapy/client-1.py
@@ -0,0 +1,4 @@
+from trapy import *
+
+conn = dial('127.0.0.1:8888')
+send(conn, b'sex 1')
--- trapy/client-2.py
+++ trapy/client-2.py
@@ -1,4 +1,4 @@
 from trapy import *
 
 conn = dial('127.0.0.1:8888')
-send(conn, b'nooooo quieeeeeerooooooooo')
+send(conn, b'sex 2')
